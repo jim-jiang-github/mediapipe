fe4215cae12534cf44d13b11fb2731c1cbd59f40
andre chen <andre.hl.chen@gmail.com>
2022-04-19 23:41:01 +0800

    cleanup

--- visualstudio/hand_tracking/hand_tracking_subgraph.cc
+++ visualstudio/hand_tracking/hand_tracking_subgraph.cc
@@ -1,15 +1,15 @@
 #include "../calculator_graph_util.h"
-DEFINE_LOAD_GRAPH("hand_tracking_desktop_live.pbtxt");
+DEFINE_LOAD_GRAPH("../../mediapipe/graphs/hand_tracking/hand_tracking_desktop_live.pbtxt");
 
 // subgraphs
 namespace mediapipe {
-DEFINE_SUBGRAPH(HandLandmarkTrackingCpu, "hand_landmark_tracking_cpu.pbtxt");
-  DEFINE_SUBGRAPH(PalmDetectionCpu, "palm_detection_cpu.pbtxt");
-    DEFINE_SUBGRAPH(PalmDetectionModelLoader, "palm_detection_model_loader.pbtxt");
-  DEFINE_SUBGRAPH(PalmDetectionDetectionToRoi, "palm_detection_detection_to_roi.pbtxt");
-  DEFINE_SUBGRAPH(HandLandmarkCpu, "hand_landmark_cpu.pbtxt");
-    DEFINE_SUBGRAPH(HandLandmarkModelLoader, "hand_landmark_model_loader.pbtxt");
-  DEFINE_SUBGRAPH(HandLandmarkLandmarksToRoi, "hand_landmark_landmarks_to_roi.pbtxt");
+DEFINE_SUBGRAPH(HandLandmarkTrackingCpu, "../../mediapipe/modules/hand_landmark/hand_landmark_tracking_cpu.pbtxt");
+  DEFINE_SUBGRAPH(PalmDetectionCpu, "../../mediapipe/modules/palm_detection/palm_detection_cpu.pbtxt");
+    DEFINE_SUBGRAPH(PalmDetectionModelLoader, "palm_detection_model_loader.pbtxt"); //../../mediapipe/modules/palm_detection 
+  DEFINE_SUBGRAPH(PalmDetectionDetectionToRoi, "../../mediapipe/modules/hand_landmark/palm_detection_detection_to_roi.pbtxt");
+  DEFINE_SUBGRAPH(HandLandmarkCpu, "hand_landmark_cpu.pbtxt"); // ../../mediapipe/modules/hand_landmark
+    DEFINE_SUBGRAPH(HandLandmarkModelLoader, "hand_landmark_model_loader.pbtxt"); // ../../mediapipe/modules/hand_landmark
+  DEFINE_SUBGRAPH(HandLandmarkLandmarksToRoi, "../../mediapipe/modules/hand_landmark/hand_landmark_landmarks_to_roi.pbtxt");
 
-DEFINE_SUBGRAPH(HandRendererSubgraph, "hand_renderer_cpu.pbtxt");
+DEFINE_SUBGRAPH(HandRendererSubgraph, "../../mediapipe/graphs/hand_tracking/subgraphs/hand_renderer_cpu.pbtxt");
 } // namespace mediapipe
